9ece54c4d13457ddfa72954e115e3a32c0d9475e
roydonGuo <3133010060@qq.com>
2023-01-12 22:39:18 +0800

    eureka

--- order-service/src/main/java/com/roydon/order/OrderApplication.java
+++ order-service/src/main/java/com/roydon/order/OrderApplication.java
@@ -3,6 +3,9 @@ package com.roydon.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.roydon.order.mapper")
 @SpringBootApplication
@@ -12,4 +15,14 @@ public class OrderApplication {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+//    @Bean
+//    public IRule randomRule() {
+//        return new RandomRule();
+//    }
 }
--- order-service/src/main/java/com/roydon/order/service/OrderService.java
+++ order-service/src/main/java/com/roydon/order/service/OrderService.java
@@ -1,8 +1,10 @@
 package com.roydon.order.service;
 
 import com.roydon.order.entity.Order;
+import com.roydon.order.entity.User;
 import com.roydon.order.mapper.OrderMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
@@ -12,9 +14,19 @@ public class OrderService {
     @Resource
     private OrderMapper orderMapper;
 
+    @Resource
+    private RestTemplate restTemplate;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+        // 2.利用Bean注册的RestTemplate发起http请求，查询用户
+//        String url = "http://localhost:8081/user/" + order.getUserId();
+        // 替换eureka中userservice服务
+        String url = "http://userservice/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        // 3.封装user到order
+        order.setUser(user);
         // 4.返回
         return order;
     }
